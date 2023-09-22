69355ad676faef2103ed65fe3827bb68409e708c
Dhilaharis <126457555+Dhilaharis@users.noreply.github.com>
2023-09-22 13:01:00 +0000

    db

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask ,render_template,jsonify
+from flask_sqlalchemy import SQLAlchemy
 # from flask_mysqldb import MySQL
-app=Flask(__name__)
 
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:ok@localhost/alchamy'
+db = SQLAlchemy(app)
 # app.config["MYSQL_HOST"]="localhost"
 # app.config["MYSQL_USER"]="root"
 # app.config["MYSQL_PASSWORD"]="mysql11"
--- database.py
+++ database.py
@@ -1,14 +1,21 @@
 from sqlalchemy import create_engine,text
-engine = create_engine("mysql+mysqlconnector://sql12647992:Cl3Vqb4Y4S@sql12.freesqldatabase.com/sql12647992") 
+# engine = create_engine("mysql+mysqlconnector://sql12647992:Cl3Vqb4Y4S@sql12.freesqldatabase.com/sql12647992") 
 # connect_args={
 #   'ssl':{  
 #     "ssl_ca":"/etc/ssl/cert.pem"
 #             }
 #          }
                        
-connection = engine.connect()
-result = connection.execute("SELECT * FROM sql12647992")  
+connection = mysql.connect()
+result = connection.execute(text("SELECT * FROM users;")) 
+
+ 
+# async with self.async_engine.connect() as con:
+#     query = "SELECT * FROM users;"
+#     result = await con.execute(text(query))
 
 # with engine.connect() as conn:
 #   result=conn.execute(text("select* from jobs"))
 #   print(result.all())
+# for row in result:
+#     print(row)
